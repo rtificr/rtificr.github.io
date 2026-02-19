fc39e22aee7a3c218c533277a2d159c0a144faac
Jay Churchill <102625530+rtificr@users.noreply.github.com>
2026-02-19 19:01:49 +0000

    Update

---
+++ pkg/gamplo_wasm_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: (a: number) => number;
+export const __wasm_bindgen_func_elem_792: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_1684: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_1912: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_2292: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_4171: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_1686: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_1915: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_2295: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_796: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
